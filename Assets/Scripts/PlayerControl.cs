7a5264dc5feaa284f6e8f86839494960cc323d7d
Zuguang Chen <airborn@qq.com>
2019-05-02 16:13:35 +1000

    Add the player flap ability
    Now player can press the space to flap the bird upward.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] float flapStrength = 1f;
+
+    Rigidbody2D myRigidbody2D; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * flapStrength;
+        }
+    }
+}
