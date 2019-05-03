b84fcf5b5ecb3f2d6936d19a44759045c1ebfa31
Zuguang Chen <airborn@qq.com>
2019-05-03 13:56:24 +1000

    Add bird animation
    the player bird have flying animation using serveral sprites.

---
+++ Assets/Scripts/Spriteanimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spriteanimator : MonoBehaviour
+{
+    [SerializeField] Sprite[] mySprites; public SpriteRenderer mySpriteRenderer; 
+    [SerializeField] float framesPerSecond = 12f; 
+    private float secondsPerFrame; 
+    private int currentFrame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>(); 
+        secondsPerFrame = 1/framesPerSecond; 
+        Invoke("NextFrame", secondsPerFrame);
+    }
+
+    void NextFrame() 
+    { 
+        currentFrame = (currentFrame + 1) % mySprites.Length; 
+        mySpriteRenderer.sprite = mySprites[currentFrame]; 
+        Invoke("NextFrame", secondsPerFrame); 
+    }
+}
