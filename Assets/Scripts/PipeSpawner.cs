e2b284abcefc10455bf88c74557d23b4daded2ea
Zuguang Chen <airborn@qq.com>
2019-05-03 10:38:32 +1000

    Add obstacle "pipe" movement and respawn
    Obstacle can now move towards the player and keep respawn

---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject pipe;
+    [SerializeField] float spawnTime; 
+    [SerializeField] float startingX; 
+    [SerializeField] float minY; 
+    [SerializeField] float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPipe();
+    }
+    void SpawnPipe() 
+    { 
+        GameObject.Instantiate( 
+            pipe, new Vector3(startingX, Random.Range(minY, maxY), 0), Quaternion.identity); 
+            Invoke("SpawnPipe", spawnTime); 
+    }
+}
