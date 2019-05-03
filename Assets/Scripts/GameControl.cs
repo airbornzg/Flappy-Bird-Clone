ed48d2728fe2f3064bb12e9523e8173d46ba7201
Zuguang Chen <airborn@qq.com>
2019-05-03 11:12:01 +1000

    Add user score function
    User can now see their score on the top left corner.

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    [SerializeField] float score = 0;
+    [SerializeField] Text myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = score + Time.deltaTime;
+        myText.text = score.ToString();
+    }
+
+    public void GameOver()
+    {
+        PlayerPrefs.SetFloat("playerScore", score);
+        //go to results scene
+    }
+}
